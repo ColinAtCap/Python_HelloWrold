4bdfaecfef5af54e46c2c21c1d7bead3abd7be0c
Colin <bhc@helenfa.plus.com>
2025-05-30 12:45:06 +0100

    Tidy and get Streamlit to read CSV

--- Files4.py
+++
@@ -1,3 +0,0 @@
-# Open a file and read its contents line by line
-with open('/Users/colinneville/Documents/PythonTemp.csv', mode='w', encoding='utf-8') as weather_file:
-    weather_file.write('Hello World!' + '\n')

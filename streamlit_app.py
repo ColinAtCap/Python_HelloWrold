c3ffdfa5da80ce5ca88b479380e1411ba73307a4
Colin <bhc@helenfa.plus.com>
2025-05-30 11:48:58 +0100

    add tuples and streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("🎈 My new app")
+    st.write(
+        "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    )
+
+main()
+# Run the app with the command: streamlit run streamlit_app.py

b7ce74ddccfeb980ae405d042036cd768ec42512
ColinAtCap <colin.neville@capgemini.com>
2025-05-30 11:42:15 +0100

    add cat

---
+++ CatBubble.py
@@ -0,0 +1,15 @@
+# Python program to display a cat bubble
+
+#comment=input('What should the cat say?')
+comment='Hello World!'
+commentlen=len(comment)
+
+print('\n')
+print('         {0}'.format('_'*commentlen))
+print('       {0} {1} {2:<3}'.format('<',comment,'>'))
+print('         {0}'.format('-'*commentlen))
+print('        /')
+print(" /\\_/\\ /")
+print('( o.o )')
+print(' > ^ <')
+print('\n')
